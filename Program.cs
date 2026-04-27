4154585bdb8a95167e787a43bf367fea904faf6c
Zerol <denisdeneroxperia@gmail.com>
2026-04-27 22:17:08 +0300

    Added functions to convert binary into both whole number and a fraction

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace Lab2
 {
@@ -6,11 +7,55 @@ namespace Lab2
     {
         static void Main()
         {
-            
+            // Get valid whole number and a fraction from the user
+            (string wholePart, string fraction) = GetValidDecimalBinary("Enter a decimal binary in [binary.binary] format");
+
+            // Convert both parts into numbers
+            int wholeNumber = ConvertBinaryToInt(wholePart);
+            double fractionNumber = ConvertFractionalBinaryToDouble(fraction);
+
+            // Add them to get final decimal result
+            double result = wholeNumber + fractionNumber;
         }
 
 
 
+        static int ConvertBinaryToInt(string binary)
+        {
+            int result = 0;
+            int multiplier = 1;
+
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                if (binary[i] == '1')
+                    result += multiplier;
+
+                multiplier *= 2;
+            }
+
+            return result;
+        }
+
+        static double ConvertFractionalBinaryToDouble(string binary)
+        {
+            double result = 0;
+            double multiplier = 0.5;
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (binary[i] == '1')
+                    result += multiplier;
+
+                multiplier /= 2;
+            }
+
+            return result;
+        }
+
+
+
+        // Prompts user until they enter a valid fraction binary
+        // Return the part before dot and the part after it
         static (string leftSide, string rightSide) GetValidDecimalBinary(string message)
         {
             while (true)
@@ -24,25 +69,30 @@ namespace Lab2
                     continue;
                 }
 
+                // Normalize and split to get the whole part and a fraction
                 string normalizedInput = input.Replace(',', '.');
                 string[] parts = normalizedInput.Split('.');
 
+                // Handle case with 2 or more dots entered
                 if (parts.Length != 2)
                 {
                     Console.WriteLine("Invalid format, please use \"binary.binary\" format");
                     continue;
                 }
 
+                // Handle any incorrect binary input
                 if (!IsBinaryValid(parts[0]) || !IsBinaryValid(parts[1]))
                 {
                     Console.WriteLine("Invalid input. Please enter a valid binary");
                     continue;
                 }
 
+                // If no errors found, return both parts
                 return (parts[0], parts[1]);
             }
         }
 
+        // Check if a string contains only valid base-2 characters
         static bool IsBinaryValid(string bin)
         {
             if (bin.Length == 0)
